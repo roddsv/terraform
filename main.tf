acb295ceccdaa0308da11bc73d9cb71b54b1593a
Rodrigo Soares <adsrodsoares@gmail.com>
2024-05-02 16:22:39 -0300

    Setting and launching EC2 Instance using Terraform

--- main.tf
+++ main.tf
@@ -10,5 +10,14 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    profile = bia
+    profile = "bia"
+}
+
+resource "aws_instance" "bia-dev" {
+    ami = "ami-02f3f602d23f1659d"
+    instance_type = "t3.micro"
+    tags = {
+      ambiente="dev"
+      Name="bia-local"
+    }
 }
